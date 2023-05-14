838d2465aed76195692cb68e7e78371d9c4bee69
Ilya Mamonchikov <imsleno@gmail.com>
2023-05-14 12:26:32 +0300

    add presets

--- js/script.js
+++ js/script.js
@@ -4,31 +4,44 @@ const COLUMNS = 16;
 
 const cellState = [];
 const initialState = [];
+const heart = [
+  [5, 5],
+  [5, 6],
+  [5, 8],
+  [5, 9],
+  [6, 4],
+  [6, 7],
+  [6, 10],
+  [7, 5],
+  [7, 9],
+  [8, 6],
+  [8, 8],
+  [9, 7],
+];
 
 const startBtn = document.getElementById("start-btn");
 const pauseBtn = document.getElementById("pause-btn");
 const stopBtn = document.getElementById("stop-btn");
+const presetsBtn = document.getElementById("presets-btn");
 
 let intervalId; // Идентификатор интервала для обновления игрового поля
 
 function resetState() {
-    // Удаляем все классы из всех клеток
-    for (let i = 0; i < ROWS * COLUMNS; i++) {
-      const cell = gameField.children[i];
-      cell.classList.remove("alive", "dead");
-    }
-  
-    // Обнуляем состояние игры
-    currentState = new Array(ROWS * COLUMNS).fill(false);
+  // Удаляем все классы из всех клеток
+  for (let i = 0; i < ROWS * COLUMNS; i++) {
+    const cell = gameField.children[i];
+    cell.classList.remove("alive", "dead");
   }
 
-
+  // Обнуляем состояние игры
+  currentState = new Array(ROWS * COLUMNS).fill(false);
+}
 
 // Создание игрового поля с клетками
 const gameField = document.getElementById("game-field");
 
 for (let i = 0; i < ROWS * COLUMNS; i++) {
-  gameField.innerHTML += "<div class='cell'></div>";
+  gameField.innerHTML += '<div class="cell"></div>';
 }
 
 // Данные о начальном состоянии клеток
@@ -40,6 +53,15 @@ for (let row = 0; row < ROWS; row++) {
     // console.log(`${row} + ${col}`);
   }
 }
+
+setTimeout(() => {
+  //рисуем сердце
+  for (let i = 0; i < ROWS - 4; i++) {
+    initialState[heart[i][0]][heart[i][1]] = true;
+    const cell = gameField.children[ROWS * heart[i][0] + heart[i][1]];
+    cell.classList.toggle("alive");
+  }
+},4000);
 
 function updateState() {
   // Создаем новый двумерный массив для хранения нового состояния клеток
@@ -105,14 +127,14 @@ function getNeighbors(row, col) {
 }
 
 function setupEventListeners() {
-    // Добавляем обработчик событий клика на каждую клетку
-    for (let i = 0; i < ROWS * COLUMNS; i++) {
-      const cell = gameField.children[i];
-      cell.addEventListener("click", () => {
-        cell.classList.toggle("alive");
-      });
-    }
+  // Добавляем обработчик событий клика на каждую клетку
+  for (let i = 0; i < ROWS * COLUMNS; i++) {
+    const cell = gameField.children[i];
+    cell.addEventListener("click", () => {
+      cell.classList.toggle("alive");
+    });
   }
+}
 
 // Отрисовываем начальную конфигурацию клеток
 //drawInitialState();
@@ -125,28 +147,57 @@ setupEventListeners();
 // }, 1000 / 30);
 
 startBtn.addEventListener("click", () => {
-    if (!intervalId) {
-      intervalId = setInterval(() => {
-        updateState();
-      }, 1000 / 5);
-    }
-  });
-  
-  pauseBtn.addEventListener("click", () => {
-    clearInterval(intervalId);
-    intervalId = undefined;
-  });
+  if (!intervalId) {
+    intervalId = setInterval(() => {
+      updateState();
+    }, 1000 / 5);
+  }
+  startBtn.classList.add("button-down");
+  pauseBtn.classList.remove("button-down");
+});
+
+pauseBtn.addEventListener("click", () => {
+  clearInterval(intervalId);
+  intervalId = undefined;
+  pauseBtn.classList.add("button-down");
+  startBtn.classList.remove("button-down");
+});
 
 //   startBtn.addEventListener("mousedown", () => {
 //     startBtn.classList.add(".button-down");
 //   });
-  
+
 //   pauseBtn.addEventListener("mousedown", () => {
 //     pauseBtn.classList.toggle(".button-down");
 //   });
-  
-  stopBtn.addEventListener("click", () => {
-    clearInterval(intervalId);
-    intervalId = undefined;
-    resetState();
-  });
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(intervalId);
+  intervalId = undefined;
+  resetState();
+  startBtn.classList.remove("button-down");
+  pauseBtn.classList.remove("button-down");
+});
+
+presetsBtn.addEventListener("click", () => {
+  clearInterval(intervalId);
+  intervalId = undefined;
+  resetState();
+  startBtn.classList.remove("button-down");
+  pauseBtn.classList.remove("button-down");
+  let i = 0;
+  for (let row = 0; row < ROWS; row++) {
+    initialState[row] = [];
+    for (let col = 0; col < COLUMNS; col++) {
+      initialState[row][col] = !!Math.round(Math.random() - 0.3);
+
+      if (initialState[row][col]) {
+        const cell = gameField.children[i];
+        cell.classList.toggle("alive");
+      }
+      i++;
+      console.log(row + col);
+      // console.log(`${row} + ${col}`);
+    }
+  }
+});
